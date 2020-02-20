2e0e4c6b0c668526888f7c4ed954e840b1b2f2a4
lhju4e <today1186@naver.com>
2020-02-20 23:16:42 +0900

    백준

--- algo/1699_re.cpp
+++ algo/1699_re.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
 using namespace std;
-
+int dp[100001];
+int po = 0;
 int main()
 {
-    
+    int n;
+    cin >> n;
+    dp[1] = 1;
+    po = 2;
+    for(int i=2; i<=n; i++)
+    {
+        if(i % (po * po) == 0)
+        {
+            dp[i] = 1;
+            po++;
+            continue;
+        }
+        dp[i] = dp[i-1] + 1;
+ 
+        for(int j=1; j*j<=i; j++)
+        {
+            dp[i] = min(dp[i-j*j]+1, dp[i]); 
+        }
+    }
+    cout << dp[n];
+    return 0;
 }
