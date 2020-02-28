1e81092f8f63741e1b212c2f5290b6ed44de89f9
lhju4e <today1186@naver.com>
2020-02-28 23:45:50 +0900

    투포인터알고리즘

--- algo/1806.cpp
+++ algo/1806.cpp
@@ -1,103 +1,37 @@
-#include <iostream>
-using namespace std;
+#include <stdio.h>
 int a[100001];
 int ans = 0;
-// void func(int);
 int n, s;
 int main()
 {
-    int MAX = 0;
-    int max_index = -1;
-    cin >> n >> s;
+    scanf("%d %d", &n, &s);
     for (int i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+    int start_index = 0;
+    int end_index = 0;
+    int b = 0;
+    int MIN = 100001;
+    while (start_index <= end_index && end_index <= n)
     {
-        cin >> a[i];
-        if (MAX < a[i])
+        printf("%d %d %d\n", start_index, end_index, b);
+        if (b >= s)
         {
-            MAX = a[i];
-            max_index = i;
-            if (MAX >= s)
-            {
-                cout << 1;
-                return 0;
-            }
+            ans = end_index - start_index;
+            if (ans < MIN)
+                MIN = ans;
+            b -= a[start_index];
+            start_index++;
         }
-    }
-    int SUM = MAX;
-    ans = 1;
-    bool i_check = true;
-    bool j_check = true;
-    int i = max_index - 1;
-    int j = max_index + 1;
-    while (i_check || j_check)
-    {
-        cout << SUM << endl;
-        if (i == -1)
-            i_check = false;
-        if (j == n)
-            j_check = false;
-        if (i_check && j_check) // i랑 j가 범위 안에 있는 경우
+        else
         {
-            if(a[i] > a[j])
-            {
-                SUM += a[i];
-                i--;
-            }
-            else
-            {
-                SUM += a[j];
-                j++;
-            }         
-        }               // 서로 비교해서 높은 값 더해줌
-        else if (i_check)  // i만 범위 안
-        {
-            SUM += a[i];
-            i--;
-        }
-        else if (j_check) 
-        {
-            SUM += a[j];
-            j++;
-        }
-            ans++;
-        if(SUM >= s)
-        {
-            cout << SUM << endl;
-            cout << ans;
-            return 0;
+            b += a[end_index];
+            end_index++;
         }
     }
 
-    // func(1);
-    cout << 0;
+    if (MIN == 100001)
+        printf("0");
+    else
+        printf("%d", MIN);
     return 0;
 }
-
-//시간초과
-// void func(int num)
-// {
-//     if(num == n)
-//         return;
-//     int sum = 0;
-//     for(int i=0; i<num; i++)
-//     {
-//         sum += a[i];
-//         if(sum > s)
-//         {
-//             ans = num;
-//             return;
-//         }
-//     }
-
-//     for(int i=0; i<n-num; i++)
-//     {
-//         sum = sum - a[i];
-//         sum = sum + a[i + num];
-//         if(sum > s)
-//         {
-//             ans = num;
-//             return;
-//         }
-//     }
-//     func(num+1);
-// }
