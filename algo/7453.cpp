f5e70996ccf9b558f2e7a3e7e4754d8763ea673d
lhju4e <today1186@naver.com>
2020-03-07 18:00:58 +0900

    브루트포스

--- algo/7453.cpp
+++ algo/7453.cpp
@@ -28,23 +28,73 @@ int main()
             cd.push_back(c[i] + d[j]);
     sort(ab.begin(), ab.end());
     sort(cd.begin(), cd.end());
+    // for(int i=0; i<ab.size(); i++)
+    //     cout << ab[i]<<" ";
+    // cout<<endl;
+    // for(int i=0; i<cd.size(); i++)
+    //     cout << cd[i]<<" ";    
+    // cout << endl;
     int i=0,j=cd.size()-1;
     //ab그룹이 음수고, cd그룹이 양수인 경우
-    while(ab[i] < 0 && cd[j] > 0)
+    while(ab[i] <= 0 && cd[j] >= 0)
     {
         if(ab[i] + cd[j] == 0)
-        {
+        {            
+        //  cout <<"i " <<i<< " ab[i] " << ab[i] << " j "<< j << " cd[j] " << cd[j] << endl;
+
             int c1 = 1;
             int c2 = 1;
             while(i<n-1 && ab[i] == ab[i+1])
             {
                 c1++;
+                i++;
             }
+            while(j>0 && cd[j] == cd[j-1])
+            {
+                c2++;
+                j--;
+            }
+            i++;j--;
             cnt += c1* c2;
         }
+        else if(ab[i] + cd[j] > 0)
+            j--;
+        else if(ab[i] + cd[j] < 0)
+            i++;
     }
-    //둘다 0인 경우
+    j=0;
+    i = ab.size()-1;
+// cout << "=============================="<< cnt << "=========="<<endl;
     //ab그룹이 양수고, cd그룹이 음수인 경우
+    while(ab[i] > 0 && cd[j] < 0)
+    {
+
+        if(ab[i] + cd[j] == 0)
+        {
+                            // cout <<"i " <<i<< " ab[i] " << ab[i] << " j "<< j << " cd[j] " << cd[j] << endl;
+
+            int c1 = 1;
+            int c2 = 1;
+            while(i>0 && ab[i] == ab[i-1])
+            {
+                c1++;
+                i--;
+            }
+            while(j<n-1 && cd[j] == cd[j+1])
+            {
+                c2++;
+                j++;
+            }
+            j++;i--;
+            cnt += c1* c2;
+        }
+        else if(ab[i] + cd[j] > 0)
+            i--;
+        else if(ab[i] + cd[j] < 0)
+            j++;
+    }
+
+
     cout << cnt;
     return 0;
 }
